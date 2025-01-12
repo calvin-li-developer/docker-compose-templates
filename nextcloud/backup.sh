6beff2edb3c9b4b1cc873bc6813e4de5c6d58ac7
Calvin Li <65045619+calvin-li-developer@users.noreply.github.com>
2025-01-11 20:41:49 -0500

    Update backup.sh

--- nextcloud/backup.sh
+++ nextcloud/backup.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
-docker stop nextcloud
+version=`docker inspect nextcloud -f '{{json .Config.Labels}}' | jq -r '.["org.opencontainers.image.version"]'`
 
-folder_path="$HOME/docker/nextcloud/data/"
+echo "Nextcloud Version: $version"
+
+echo "Stop Nextcloud docker ..." && docker stop nextcloud nextcloud-database > /dev/null
+
+echo "Backup starting ..."
+
+backup_paths="README.md docker-compose.yml cloudflared-config fail2ban-config .env data/$NEXTCLOUD_USER/files"
+folder_path="$HOME/docker/nextcloud"
 target_dir="$HOME/docker/nextcloud/backups"
 max_num_backups=1
-zip_file="$target_dir/nc-$(date +'%Y-%m-%d_%I%M_%p').tar.gz"
-tar -czf "$zip_file" -C "$folder_path" . > /dev/null
+current_date=`date +'%Y-%m-%d_%I%M_%p'`
 
-echo "[INFO]: tar zip file location is $target_dir"
+mkdir -p "$target_dir/${version%%-*}"
+find $target_dir -mindepth 1 -maxdepth 1 -type d ! -name "${version%%-*}" -exec rm -rf {} +
 
-if [ "$(ls -1qA $target_dir | grep nc | wc -l)" -gt $max_num_backups ]; then
+zip_file="$target_dir/${version%%-*}/nc-$current_date.tar.gz"
+
+echo "Backup compressed filename: $zip_file"
+sudo tar -czf "$zip_file" -C "$folder_path" $backup_paths > /dev/null
+
+if [ "$(ls -1qAR $target_dir | grep nc | wc -l)" -gt $max_num_backups ]; then
     # Get the oldest file in the directory
-    oldest_file=$(ls -1t $target_dir | grep "nc-" | tail -1 | awk '{print $NF}')
-    echo "[INFO]: Backup tar files in $target_dir exceeded $max_num_backups"
-    echo "[INFO]: Oldest file removed is $oldest_file"
+    oldest_file="$(ls -1tR $target_dir | grep nc | tail -1 | awk '{print $NF}')"
+    echo "Backup tar files in $target_dir exceeded $max_num_backups"
+    echo "Oldest file removed is $oldest_file"
     # Delete the oldest file
-    rm $target_dir/$oldest_file
+    rm "$target_dir/${version%%-*}/$oldest_file"
 fi
 
-docker start nextcloud
+echo "Start Nextcloud docker ..." && docker start nextcloud nextcloud-database
